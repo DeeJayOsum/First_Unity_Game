b4c92158bc90a243197b916244fa5eb26f7365b1
Arjun-Sunil <44163725+Arjun-Sunil@users.noreply.github.com>
2020-01-15 20:21:07 +0530

    Add files via upload

---
+++ move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour {
+	public float moveSpeed = 10f;
+	private Rigidbody rbody;
+	void Start () {
+		rbody=GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+
+		float inputX = Input.GetAxis("Horizontal");
+		float inputZ = Input.GetAxis("Vertical");
+
+		float moveX = inputX*moveSpeed*Time.deltaTime;
+		float moveZ = inputZ*moveSpeed*Time.deltaTime;
+
+		rbody.AddForce(moveX,0f,moveZ);
+
+
+	}
+}
+
+
+
